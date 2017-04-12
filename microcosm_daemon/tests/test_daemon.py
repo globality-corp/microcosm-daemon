f28929864f0906a3af6d9a47452d45d812bd210a
Jesse Myers <jesse.myers@globality.com>
2017-04-12 09:42:00 -0700

    Add a simple hook for testing daemon initialization.
    Main changes:
     -  Separate daemon initialization from startup
     -  Add a factory classmethod for testing

---
+++ microcosm_daemon/tests/test_daemon.py
@@ -0,0 +1,28 @@
+"""
+Test daemon loading.
+
+"""
+from hamcrest import assert_that, equal_to, is_
+
+from microcosm_daemon.daemon import Daemon
+
+
+class FixtureDaemon(Daemon):
+
+    @property
+    def name(self):
+        return "fixture"
+
+    @property
+    def components(self):
+        return super(FixtureDaemon, self).components + [
+            "hello_world",
+        ]
+
+    def __call__(self):
+        pass
+
+
+def test_daemon_initialize():
+    daemon = FixtureDaemon.create_for_testing()
+    assert_that(daemon.graph.hello_world, is_(equal_to("hello world")))
